542b0299c8ed60ea817897826ea0532e6dfc7962
thiagoxm <thiagoxm@gmail.com>
2026-03-16 22:14:30 -0300

    projeto inicial

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Cultivoo WebApp',
+        short_name: 'Cultivoo',
+        description: 'Seu primeiro app agro!',
+        theme_color: '#ffffff',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+})
